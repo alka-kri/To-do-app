96980376ff779480384b16e73aec94bbacdfe0a0
Alka Kumari <alkakumarimnd@gmail.com>
2024-03-27 14:19:22 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# ToDo App
+
+## Description
+This is a simple To Do App, built to practice DOM. Have fun while interacting with the elements.
+You can add todo, remove todo and search lists of todos.
+
+## Installation
+Clone the repository and cd into the project directory  
+Open with live server or open index.html file in your browser  
+
+## Usage
+Add todo to existing hobbies
+Delete any todo that does not define you by clicking on the 'X' button  
+Use the search box to filter the list of todos based on search input  
+Play with the box by hovering your mouse on it to see effect  
+You can click 'Refresh' to refresh page  
+
+## Built With
+* HTML - For the markup  
+* CSS - For styling the user interface  
+* JavaScript - For functionality  
+
+## Link
+Access app via https://dinakajoy.github.io/ToDo-app/
+
+## Author
+[Odinaka Joy](http://dinakajoy.com)  
---
+++ main.js
@@ -0,0 +1,77 @@
+const submitTodo = document.querySelector('#submitTodo');
+const addTodo = document.querySelector('#addTodo');
+const errorOutput = document.querySelector('#output');
+const todos = document.querySelector('#todos');
+const filterTodo = document.querySelector('#search');
+const allTodos = document.querySelectorAll('li');
+
+const addTodoFunc = (e) => {
+  e.preventDefault();
+  if(addTodo === '' || addTodo.value.length < 5) {
+    errorOutput.textContent = 'Please Enter A Minimum Of Three Characters';
+    errorOutput.style.backgroundColor = 'red';
+    errorOutput.style.color = 'white';
+    errorOutput.style.padding = '2px 3px';
+  } else {
+    errorOutput.textContent = '';
+    errorOutput.style.padding = '0';
+
+    let delButton = document.createElement('button');
+    delButton.className = 'delete';
+    delButton.appendChild(document.createTextNode('x'));
+  
+    let newTodo = document.createElement('li');
+    newTodo.appendChild(document.createTextNode(addTodo.value));
+    newTodo.appendChild(delButton);
+    todos.appendChild(newTodo);
+    addTodo.value = '';
+  }
+};
+
+
+
+const filterTodoFunc = (e) => {
+  e.preventDefault();
+  let searchValue = e.target.value.toLowerCase();
+  allTodos.forEach(todo => {
+    let currentTodo = todo.firstChild.textContent.toLowerCase();
+    if(currentTodo.indexOf(searchValue) !== -1) {
+      todo.style.display = 'block';
+    } else {
+      todo.style.display = 'none';
+    }
+  });
+}
+
+submitTodo.addEventListener('click', addTodoFunc);
+todos.addEventListener('click', delTodoFunc);
+filterTodo.addEventListener('keyup', filterTodoFunc);
+
+//Change Background Color Function
+const changeBg = document.querySelector('#changeBg');
+const sectionB = document.querySelector('.section-b');
+const reload = document.querySelector('#reload');
+
+const delTodoFunc = (e) => {
+  if(e.target.className === "delete") {
+    if(confirm('Are You Sure You Want To Delete Todo?')) {
+      todos.removeChild(e.target.parentElement);
+    }
+  }
+}
+
+const changeBodyBg = (e) => {
+  sectionB.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 50)`;
+}
+
+const resetBodyBg = () => {
+  sectionB.style.backgroundColor = '#f4f4f4';
+}
+
+reload.addEventListener('click', (e) => {
+  e.preventDefault();
+  location.reload();
+})
+
+changeBg.addEventListener('mousemove', changeBodyBg);
+changeBg.addEventListener('mouseout', resetBodyBg);
